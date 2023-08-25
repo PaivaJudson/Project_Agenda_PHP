36233c41755c2d802cfa04eeffba453d080a80a6
Judson Paiva <judson.paiva@hotmail.com>
2023-08-25 23:39:03 +0100

    templates e definicoes gerais

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+    include_once("templates/header.php");
+
+?>
+
+    
+
+<?php
+
+    include_once("templates/footer.php");
+
+?>
---
+++ templates/header.php
@@ -0,0 +1,25 @@
+<?php
+
+    include_once("config/url.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de Contactos</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+</head>
+
+<body>
+
+<head>
+    <nav>
+        
+    </nav>
+</head>
